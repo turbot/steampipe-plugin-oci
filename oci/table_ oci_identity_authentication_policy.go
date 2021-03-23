bbbed63c8ff37fafc790d5c189a7139eb218b746
LalitTurbot <lalit@turbot.com>
2021-03-23 12:34:44 +0530

    Add table oci_identity_authentication_policy

---
+++ oci/table_ oci_identity_authentication_policy.go
@@ -0,0 +1,104 @@
+package oci
+
+import (
+	"context"
+
+	oci_common "github.com/oracle/oci-go-sdk/v36/common"
+	"github.com/oracle/oci-go-sdk/v36/identity"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableIdentityAuthenticationPolicy(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "oci_identity_authentication_policy",
+		Description: "OCI Identity Authentication Policy",
+		List: &plugin.ListConfig{
+			Hydrate: listAuthenticationPolicy,
+		},
+		Columns: []*plugin.Column{
+			// Password Policy
+			{
+				Name:        "is_lowercase_characters_required",
+				Description: "At least one lower case character required.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("PasswordPolicy.IsLowercaseCharactersRequired"),
+			},
+			{
+				Name:        "is_numeric_characters_required",
+				Description: "At least one numeric character required.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("PasswordPolicy.IsNumericCharactersRequired"),
+			},
+			{
+				Name:        "is_special_characters_required",
+				Description: "At least one special character required.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("PasswordPolicy.IsSpecialCharactersRequired"),
+			},
+			{
+				Name:        "is_uppercase_characters_required",
+				Description: "At least one uppercase character required.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("PasswordPolicy.IsUppercaseCharactersRequired"),
+			},
+			{
+				Name:        "is_username_containment_allowed",
+				Description: "User name is allowed to be part of the password.",
+				Type:        proto.ColumnType_BOOL,
+				Transform:   transform.FromField("PasswordPolicy.IsUsernameContainmentAllowed"),
+			},
+			{
+				Name:        "minimum_password_length",
+				Description: "Minimum password length required.",
+				Type:        proto.ColumnType_INT,
+				Transform:   transform.FromField("PasswordPolicy.MinimumPasswordLength"),
+			},
+
+			// Network Policy
+			{
+				Name:        "network_source_ids",
+				Description: "List of network source ids.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("NetworkPolicy.NetworkSourceIds"),
+			},
+
+			// Standard OCI columns
+			{
+				Name:        "tenant_id",
+				Description: ColumnDescriptionTenant,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("CompartmentId"),
+			},
+		},
+	}
+}
+
+//// LIST FUNCTION
+
+func listAuthenticationPolicy(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	// Create Session
+	session, err := identityService(ctx, d)
+	if err != nil {
+		return nil, err
+	}
+
+	// The OCID of the tenancy containing the compartment.
+	request := identity.GetAuthenticationPolicyRequest{
+		CompartmentId: &session.TenancyID,
+		RequestMetadata: oci_common.RequestMetadata{
+			RetryPolicy: getDefaultRetryPolicy(),
+		},
+	}
+
+	response, err := session.IdentityClient.GetAuthenticationPolicy(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	d.StreamListItem(ctx, response.AuthenticationPolicy)
+	return nil, err
+}

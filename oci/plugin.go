a721e1a0348bab3119270779d3b1071f6176acf9
Rajesh Bal <46851617+rajeshbal65@users.noreply.github.com>
2021-04-23 01:04:02 +0530

    Add table oci_core_nat_gateway. Closes #41 (#45)

--- oci/plugin.go
+++ oci/plugin.go
@@ -33,6 +33,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"oci_core_vcn":                       tableCoreVcn(ctx),
 			"oci_core_internet_gateway":          tableCoreInternetGateway(ctx),
 			"oci_core_local_peering_gateway":     tableCoreLocalPeeringGateway(ctx),
+			"oci_core_nat_gateway":               tableCoreNatGateway(ctx),
 			"oci_core_route_table":               tableCoreRouteTable(ctx),
 			"oci_core_volume":                    tableCoreVolume(ctx),
 			"oci_core_volume_backup":             tableCoreVolumeBackup(ctx),

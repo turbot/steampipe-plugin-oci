e22c70b87ea634dcf337227c45d8fcc2e345b236
bigdatasourav <bigdatasourav@gmail.com>
2021-06-16 16:11:05 +0530

    test update

--- oci-test/tests/oci_identity_tag_default/variables.tf
+++ oci-test/tests/oci_identity_tag_default/variables.tf
@@ -12,7 +12,6 @@ variable "config_file_profile" {
 
 variable "tenancy_ocid" {
   type        = string
-  default     = "ocid1.tenancy.oc1..aaaaaaaahnm7gleh5soecxzjetci3yjjnjqmfkr4po3hoz4p4h2q37cyljaq"
   description = "OCID of your tenancy."
 }
 
@@ -22,30 +21,25 @@ variable "region" {
   description = "OCI region used for the test. Does not work with default region in config, so must be defined here."
 }
 
-variable "tag_namespace_description" {
-  type        = string
-  default     = "Terraform testing resource"
-  description = "The description you assign to the tag namespace."
-}
-
 provider "oci" {
   tenancy_ocid        = var.tenancy_ocid
   config_file_profile = var.config_file_profile
   region              = var.region
 }
 
-resource "oci_identity_tag_namespace" "named_test_resource" {
-  compartment_id = var.tenancy_ocid
-  description    = var.tag_namespace_description
-  name           = var.resource_name
+locals {
+  path = "${path.cwd}/output.json"
 }
 
-resource "oci_identity_tag_default" "named_test_resource" {
-  depends_on = [oci_identity_tag_namespace.named_test_resource]
-  #Required
-  compartment_id    = var.tenancy_ocid
-  tag_definition_id = oci_identity_tag_namespace.named_test_resource.id
-  value             = "test"
+resource "null_resource" "named_test_resource" {
+  provisioner "local-exec" {
+    command = "oci iam tag-default list --all --compartment-id ${var.tenancy_ocid} --output json > ${local.path}"
+  }
+}
+
+data "local_file" "input" {
+  depends_on = [null_resource.named_test_resource]
+  filename   = local.path
 }
 
 output "tenancy_ocid" {
@@ -53,5 +47,6 @@ output "tenancy_ocid" {
 }
 
 output "resource_id" {
-  value = oci_identity_tag_default.named_test_resource.id
+  depends_on = [null_resource.named_test_resource]
+  value      = jsondecode(data.local_file.input.content).data[0].id
 }

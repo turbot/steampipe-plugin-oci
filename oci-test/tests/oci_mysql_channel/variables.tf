9282f1638e9425f780321fd0e915975c4829ff25
bigdatasourav <bigdatasourav@gmail.com>
2021-07-13 13:12:46 +0530

    update

--- oci-test/tests/oci_mysql_channel/variables.tf
+++ oci-test/tests/oci_mysql_channel/variables.tf
@@ -22,54 +22,55 @@ variable "region" {
 }
 
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
+  tenancy_ocid        = var.tenancy_ocid
   config_file_profile = var.config_file_profile
 }
 
 resource "oci_core_vcn" "named_test_resource" {
-    compartment_id = var.tenancy_ocid
-    cidr_block = "10.0.0.0/16"
+  compartment_id = var.tenancy_ocid
+  cidr_block     = "10.0.0.0/16"
 }
+
 resource "oci_core_subnet" "named_test_resource" {
-    cidr_block = "10.0.0.0/16"
-    compartment_id = var.tenancy_ocid
-    vcn_id = oci_core_vcn.named_test_resource.id
+  cidr_block     = "10.0.0.0/16"
+  compartment_id = var.tenancy_ocid
+  vcn_id         = oci_core_vcn.named_test_resource.id
 }
 
 resource "oci_mysql_mysql_db_system" "named_test_resource" {
-    #Required
-    admin_password ="Admin@1234"
-    admin_username = "admin"
-    availability_domain = "TvRS:AP-MUMBAI-1-AD-1"
-    compartment_id = var.tenancy_ocid
-    shape_name = "VM.Standard.E2.1"
-    subnet_id = oci_core_subnet.named_test_resource.id
-    display_name = var.resource_name
-    data_storage_size_in_gb = "50"
+  #Required
+  admin_password          = "Admin@1234"
+  admin_username          = "admin"
+  availability_domain     = "TvRS:AP-MUMBAI-1-AD-1"
+  compartment_id          = var.tenancy_ocid
+  shape_name              = "VM.Standard.E2.1"
+  subnet_id               = oci_core_subnet.named_test_resource.id
+  display_name            = var.resource_name
+  data_storage_size_in_gb = "50"
 }
 
 resource "oci_mysql_channel" "named_test_resource" {
+  #Required
+  source {
     #Required
-    source {
-        #Required
-        hostname = var.resource_name
-        password = "Test@1234"
-        source_type = "MYSQL"
-        ssl_mode = "REQUIRED"
-        username = var.resource_name
-    }
+    hostname    = var.resource_name
+    password    = "Test@1234"
+    source_type = "MYSQL"
+    ssl_mode    = "REQUIRED"
+    username    = var.resource_name
+  }
 
-    target {
-        #Required
-        db_system_id = oci_mysql_mysql_db_system.named_test_resource.id
-        target_type = "DBSYSTEM"
-    }
+  target {
+    #Required
+    db_system_id = oci_mysql_mysql_db_system.named_test_resource.id
+    target_type  = "DBSYSTEM"
+  }
 
-    #Optional
-    description = var.resource_name
-    display_name = var.resource_name
-    freeform_tags = {"name"= var.resource_name}
-    is_enabled = true
+  #Optional
+  description   = var.resource_name
+  display_name  = var.resource_name
+  freeform_tags = { "name" = var.resource_name }
+  is_enabled    = true
 }
 
 output "resource_name" {

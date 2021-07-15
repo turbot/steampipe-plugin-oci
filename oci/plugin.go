6baf7ef651b542e5b18f25d150a77b0df1e477ea
Rajesh Bal <46851617+rajeshbal65@users.noreply.github.com>
2021-07-16 01:03:03 +0530

    Add table oci_dns_rrset. Closes #97 (#112)

--- oci/plugin.go
+++ oci/plugin.go
@@ -53,6 +53,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"oci_core_volume_backup":                     tableCoreVolumeBackup(ctx),
 			"oci_core_volume_backup_policy":              tableCoreVolumeBackupPolicy(ctx),
 			"oci_database_autonomous_database":           tableDatabaseAutonomousDatabase(ctx),
+			"oci_dns_rrset":                              tableDnsRecordSet(ctx),
 			"oci_dns_tsig_key":                           tableDnsTsigKey(ctx),
 			"oci_dns_zone":                               tableDnsZone(ctx),
 			"oci_events_rule":                            tableEventsRule(ctx),

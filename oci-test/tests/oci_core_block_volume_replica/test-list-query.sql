49662f45a9adc57630bd7c00d01ee2594964c9d6
bigdatasourav <bigdatasourav@gmail.com>
2021-07-16 14:34:49 +0530

    oci_sdk_update

--- oci-test/tests/oci_core_block_volume_replica/test-list-query.sql
+++
@@ -1,3 +0,0 @@
-select id, display_name, lifecycle_state
-from oci.oci_core_block_volume_replica
-where display_name = '{{ resourceName }}';
--- oci-test/tests/oci_core_block_volume_replica/test-not-found-query.sql
+++
@@ -1,3 +0,0 @@
-select id, lifecycle_state
-from oci.oci_core_block_volume_replica
-where id = '{{ output.resource_id.value }}::dummy';

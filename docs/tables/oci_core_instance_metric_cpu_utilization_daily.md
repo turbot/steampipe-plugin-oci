d6baab27e517f7eecdd1155eca21a6778edf288c
ParthaI <partha@turbot.com>
2021-07-16 12:48:09 +0530

    Add table oci_core_instance_metric_cpu_utilization_daily closes #190

---
+++ docs/tables/oci_core_instance_metric_cpu_utilization_daily.md
@@ -0,0 +1,43 @@
+# Table: oci_core_instance_metric_cpu_utilization_daily
+
+OCI Monitoring Metrics Explorer provide data about the performance of your systems.  The `oci_core_instance_metric_cpu_utilization_daily` table provides metric statistics at 24 hour intervals for the most recent 90 days.
+
+
+## Examples
+
+### Basic info
+
+```sql
+select
+  instance_id,
+  timestamp,
+  minimum,
+  maximum,
+  average,
+  sample_count
+from
+  oci_core_instance_metric_cpu_utilization_daily
+order by
+  instance_id,
+  timestamp;
+```
+
+
+
+### CPU Over 80% average
+
+```sql
+select
+  instance_id,
+  timestamp,
+  round(minimum::numeric,2) as min_cpu,
+  round(maximum::numeric,2) as max_cpu,
+  round(average::numeric,2) as avg_cpu,
+  sample_count
+from
+  oci_core_instance_metric_cpu_utilization_daily
+where average > 8
+order by
+  instance_id,
+  timestamp;
+```
---
+++ oci/table_oci_core_instance_metric_cpu_utilization_daily.go
@@ -0,0 +1,37 @@
+package oci
+
+import (
+	"context"
+
+	"github.com/oracle/oci-go-sdk/v36/core"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+func tableOciCoreInstanceMetricCpuUtilizationDaily(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "oci_core_instance_metric_cpu_utilization_daily",
+		Description: "OCI Core Instance Monitoring Metrics - CPU Utilization (Daily)",
+		List: &plugin.ListConfig{
+			ParentHydrate: listCoreInstances,
+			Hydrate:       listCoreInstanceMetricCpuUtilizationDaily,
+		},
+		GetMatrixItem: BuildCompartementRegionList,
+		Columns: MonitoringMetricColumns(
+			[]*plugin.Column{
+				{
+					Name:        "instance_id",
+					Description: "The Core Instance ID.",
+					Type:        proto.ColumnType_STRING,
+					Transform:   transform.FromField("DimensionValue"),
+				},
+			}),
+	}
+}
+
+func listCoreInstanceMetricCpuUtilizationDaily(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	instance := h.Item.(core.Instance)
+	return listMonitoringMetricStastics(ctx, d, "DAILY", "oci_computeagent", "CpuUtilization", "resourceId", *instance.Id, *instance.CompartmentId)
+}

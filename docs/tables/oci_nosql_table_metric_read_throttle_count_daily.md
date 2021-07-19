60f31ab51906425dc80fc420be6fb265fcc65c67
ParthaI <partha@turbot.com>
2021-07-19 12:47:19 +0530

    Added metric statistic table for nosql table read throttle count #219 #218

---
+++ docs/tables/oci_nosql_table_metric_read_throttle_count_daily.md
@@ -0,0 +1,43 @@
+# Table: oci_nosql_table_metric_read_throttle_count_daily
+
+OCI Monitoring Metrics provide data about the performance of your systems.  The `oci_nosql_table_metric_read_throttle_count_daily` table provides metric statistics at 24 hour intervals for the most recent 90 days.
+
+
+## Examples
+
+### Basic info
+
+```sql
+select
+  table_name,
+  timestamp,
+  minimum,
+  maximum,
+  average,
+  sum,
+  sample_count
+from
+  oci_nosql_table_metric_read_throttle_count_daily
+order by
+  table_name,
+  timestamp;
+```
+
+### Intervals where read throttle count exceded 100 average
+```sql
+select
+  table_name,
+  timestamp,
+  minimum,
+  maximum,
+  average,
+  sum,
+  sample_count
+from
+  oci_nosql_table_metric_read_throttle_count_daily
+where
+  average > 100
+order by
+  table_name,
+  timestamp;
+```
---
+++ oci/table_oci_nosql_table_metric_read_throttle_count_hourly.go
@@ -0,0 +1,37 @@
+package oci
+
+import (
+	"context"
+
+	"github.com/oracle/oci-go-sdk/v36/nosql"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+func tableOciNoSQLTableMetricReadThrottleCountHourly(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "oci_nosql_table_metric_read_throttle_count_hourly",
+		Description: "OCI NoSQL Table Monitoring Metrics - Read Throttle Count (Hourly)",
+		List: &plugin.ListConfig{
+			ParentHydrate: listNoSQLTables,
+			Hydrate:       listNoSQLTableMetricReadThrottleCountHourly,
+		},
+		GetMatrixItem: BuildCompartementRegionList,
+		Columns: MonitoringMetricColumns(
+			[]*plugin.Column{
+				{
+					Name:        "table_name",
+					Description: "The NoSQL Table Name.",
+					Type:        proto.ColumnType_STRING,
+					Transform:   transform.FromField("DimensionValue"),
+				},
+			}),
+	}
+}
+
+func listNoSQLTableMetricReadThrottleCountHourly(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	table := h.Item.(nosql.TableSummary)
+	return listMonitoringMetricStastics(ctx, d, "HOURLY", "oci_nosql", "ReadThrottleCount", "tableName", *table.Name, *table.CompartmentId)
+}

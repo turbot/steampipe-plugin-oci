856640967e403a3bf8dfd6bedde31a3344d90a63
ParthaI <partha@turbot.com>
2021-07-23 15:27:50 +0530

    Add table oci_database_autonomous_database_metric_cpu_utilization closes #252

---
+++ oci/table_oci_database_autonomous_database_cpu_utilization.go
@@ -0,0 +1,40 @@
+package oci
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/oracle/oci-go-sdk/v44/database"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableDatabaseAutonomousDatabaseMetricCpuUtilization(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "oci_database_autonomous_database_metric_cpu_utilization",
+		Description: "OCI Autonomous Database Monitoring Metrics - CPU Utilization",
+		List: &plugin.ListConfig{
+			ParentHydrate: listAutonomousDatabases,
+			Hydrate:       listAutonomousDatabaseMetricCpuUtilization,
+		},
+		GetMatrixItem: BuildCompartementRegionList,
+		Columns: MonitoringMetricColumns(
+			[]*plugin.Column{
+				{
+					Name:        "id",
+					Description: "The OCID of the Autonomous Database.",
+					Type:        proto.ColumnType_STRING,
+					Transform:   transform.FromField("DimensionValue"),
+				},
+			}),
+	}
+}
+
+func listAutonomousDatabaseMetricCpuUtilization(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	database := h.Item.(database.AutonomousDatabaseSummary)
+	region := fmt.Sprintf("%v", ociRegionNameFromId(*database.Id))
+	return listMonitoringMetricStatistics(ctx, d, "5_MIN", "oci_autonomous_database", "CpuUtilization", "resourceId", *database.Id, *database.CompartmentId, region)
+}

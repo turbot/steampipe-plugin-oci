662c1bbe2a74d9fcab072a3883f3e0e0e4c78eea
bigdatasourav <bigdatasourav@gmail.com>
2021-08-04 12:41:39 +0530

    update

--- oci/table_oci_file_storage_file_system.go
+++ oci/table_oci_file_storage_file_system.go
@@ -185,12 +185,11 @@ func listFileStorageFileSystems(ctx context.Context, d *plugin.QueryData, _ *plu
 //// HYDRATE FUNCTION
 
 func getFileStorageFileSystem(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-	plugin.Logger(ctx).Trace("getFileStorageFileSystem")
 	logger := plugin.Logger(ctx)
 	region := plugin.GetMatrixItem(ctx)[matrixKeyRegion].(string)
 	zone := plugin.GetMatrixItem(ctx)[matrixKeyZone].(string)
 	compartment := plugin.GetMatrixItem(ctx)[matrixKeyCompartment].(string)
-	logger.Debug("getFunctionsApplication", "Compartment", compartment, "OCI_ZONE", zone)
+	logger.Debug("getFileStorageFileSystem", "Compartment", compartment, "OCI_ZONE", zone)
 
 	var id string
 	if h.Item != nil {
@@ -199,9 +198,9 @@ func getFileStorageFileSystem(ctx context.Context, d *plugin.QueryData, h *plugi
 	} else {
 		id = d.KeyColumnQuals["id"].GetStringValue()
 		// Restrict the api call to only root compartment and one zone/ per region
-	if !strings.HasPrefix(compartment, "ocid1.tenancy.oc1") || !strings.HasSuffix(zone, "AD-1") {
-		return nil, nil
-	}
+		if !strings.HasPrefix(compartment, "ocid1.tenancy.oc1") || !strings.HasSuffix(zone, "AD-1") {
+			return nil, nil
+		}
 	}
 
 	// handle empty application id in get call

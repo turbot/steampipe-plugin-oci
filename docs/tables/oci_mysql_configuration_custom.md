4105db7f09f9ea2f26e7fad6903a7423fecf68d3
Arnab Ghosh <arnabg57@gmail.com>
2021-08-18 15:50:53 +0530

    Add table oci_mysql_configuration_custom

---
+++ docs/tables/oci_mysql_configuration_custom.md
@@ -0,0 +1,33 @@
+# Table: oci_mysql_configuration_custom
+
+DB System configurations are collections of variables which define the operation of the MySQL DB System.
+
+## Examples
+
+### Basic info
+
+```sql
+select
+  display_name,
+  id,
+  description,
+  lifecycle_state as state,
+  time_created
+from
+  oci_mysql_configuration_custom;
+```
+
+### List deleted configurations
+
+```sql
+select
+  display_name,
+  id,
+  description,
+  lifecycle_state as state,
+  time_created
+from
+  oci_mysql_configuration_custom 
+where
+  lifecycle_state = 'DELETED';
+```
---
+++ oci/table_oci_mysql_configuration_custom.go
@@ -0,0 +1,224 @@
+package oci
+
+import (
+    "context"
+    "strings"
+
+    "github.com/oracle/oci-go-sdk/v44/common"
+    "github.com/oracle/oci-go-sdk/v44/mysql"
+    "github.com/turbot/go-kit/types"
+    "github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+    "github.com/turbot/steampipe-plugin-sdk/plugin"
+    "github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableMySQLCustomConfiguration(_ context.Context) *plugin.Table {
+    return &plugin.Table{
+        Name:        "oci_mysql_configuration_custom",
+        Description: "OCI MySQL Custom Configuration",
+        Get: &plugin.GetConfig{
+            KeyColumns: plugin.SingleColumn("id"),
+            Hydrate:    getCustomConfiguration,
+        },
+        List: &plugin.ListConfig{
+            Hydrate: listMySQLCustomConfigurations,
+        },
+        GetMatrixItem: BuildCompartementRegionList,
+        Columns: []*plugin.Column{
+            {
+                Name:        "display_name",
+                Description: "The display name of the Configuration.",
+                Type:        proto.ColumnType_STRING,
+            },
+            {
+                Name:        "id",
+                Description: "The OCID of the Configuration.",
+                Type:        proto.ColumnType_STRING,
+                Transform:   transform.FromCamel(),
+            },
+            {
+                Name:        "parent_configuration_id",
+                Description: "The OCID of the Configuration from which this Configuration is derived.",
+                Type:        proto.ColumnType_STRING,
+                Hydrate:     getCustomConfiguration,
+                Transform:   transform.FromField("ParentConfigurationId"),
+            },
+            {
+                Name:        "lifecycle_state",
+                Description: "The current state of the Configuration.",
+                Type:        proto.ColumnType_STRING,
+            },
+            {
+                Name:        "time_created",
+                Description: "The date and time the Configuration was created.",
+                Type:        proto.ColumnType_TIMESTAMP,
+                Transform:   transform.FromField("TimeCreated.Time"),
+            },
+
+            // other columns
+            {
+                Name:        "description",
+                Description: "User-provided data about the Configuration.",
+                Type:        proto.ColumnType_STRING,
+            },
+            {
+                Name:        "shape_name",
+                Description: "The name of the associated Shape.",
+                Type:        proto.ColumnType_STRING,
+            },
+            {
+                Name:        "time_updated",
+                Description: "The date and time the Configuration was last updated.",
+                Type:        proto.ColumnType_TIMESTAMP,
+                Transform:   transform.FromField("TimeUpdated.Time"),
+            },
+            {
+                Name:        "type",
+                Description: "The Configuration type, DEFAULT or CUSTOM.",
+                Type:        proto.ColumnType_STRING,
+            },
+            {
+                Name:        "variables",
+                Description: "User controllable service variables of the Configuration.",
+                Type:        proto.ColumnType_STRING,
+                Hydrate:     getCustomConfiguration,
+            },
+
+            // tags
+            {
+                Name:        "defined_tags",
+                Description: ColumnDescriptionDefinedTags,
+                Type:        proto.ColumnType_JSON,
+            },
+            {
+                Name:        "freeform_tags",
+                Description: ColumnDescriptionFreefromTags,
+                Type:        proto.ColumnType_JSON,
+            },
+
+            // Steampipe standard columns
+            {
+                Name:        "tags",
+                Description: ColumnDescriptionTags,
+                Type:        proto.ColumnType_JSON,
+                Transform:   transform.From(configurationTags),
+            },
+            {
+                Name:        "title",
+                Description: ColumnDescriptionTitle,
+                Type:        proto.ColumnType_STRING,
+                Transform:   transform.FromField("DisplayName"),
+            },
+
+            // OCI standard columns
+            {
+                Name:        "region",
+                Description: ColumnDescriptionRegion,
+                Type:        proto.ColumnType_STRING,
+                Transform:   transform.FromField("Id").Transform(ociRegionName),
+            },
+            {
+                Name:        "compartment_id",
+                Description: ColumnDescriptionCompartment,
+                Type:        proto.ColumnType_STRING,
+                Transform:   transform.FromField("CompartmentId"),
+            },
+            {
+                Name:        "tenant_id",
+                Description: ColumnDescriptionTenant,
+                Type:        proto.ColumnType_STRING,
+                Hydrate:     getTenantId,
+                Transform:   transform.FromValue(),
+            },
+        },
+    }
+}
+
+//// LIST FUNCTION
+
+func listMySQLCustomConfigurations(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+    logger := plugin.Logger(ctx)
+    region := plugin.GetMatrixItem(ctx)[matrixKeyRegion].(string)
+    compartment := plugin.GetMatrixItem(ctx)[matrixKeyCompartment].(string)
+    logger.Debug("listMySQLConfigurations", "Compartment", compartment, "OCI_REGION", region)
+
+    // Create Session
+    session, err := mySQLConfigurationService(ctx, d, region)
+    if err != nil {
+        return nil, err
+    }
+
+    request := mysql.ListConfigurationsRequest{
+        CompartmentId: types.String(compartment),
+		Type:          []mysql.ListConfigurationsTypeEnum{"CUSTOM"},
+        RequestMetadata: common.RequestMetadata{
+            RetryPolicy: getDefaultRetryPolicy(),
+        },
+    }
+
+    pagesLeft := true
+    for pagesLeft {
+        response, err := session.MySQLConfigurationClient.ListConfigurations(ctx, request)
+        if err != nil {
+            return nil, err
+        }
+        for _, configuration := range response.Items {
+            d.StreamListItem(ctx, configuration)
+        }
+        if response.OpcNextPage != nil {
+            request.Page = response.OpcNextPage
+        } else {
+            pagesLeft = false
+        }
+    }
+
+    return nil, err
+}
+
+//// HYDRATE FUNCTION
+
+func getCustomConfiguration(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+    logger := plugin.Logger(ctx)
+    region := plugin.GetMatrixItem(ctx)[matrixKeyRegion].(string)
+    compartment := plugin.GetMatrixItem(ctx)[matrixKeyCompartment].(string)
+    logger.Debug("getCustomConfiguration", "Compartment", compartment, "OCI_REGION", region)
+
+    var id string
+    if h.Item != nil {
+        configuration := h.Item.(mysql.ConfigurationSummary)
+        id = *configuration.Id
+    } else {
+        id = d.KeyColumnQuals["id"].GetStringValue()
+        // Restrict the api call to only root compartment/ per region
+        if !strings.HasPrefix(compartment, "ocid1.tenancy.oc1") {
+            return nil, nil
+        }
+    }
+
+    // handle empty id in get call
+    if strings.TrimSpace(id) == "" {
+        return nil, nil
+    }
+
+    // Create Session
+    session, err := mySQLConfigurationService(ctx, d, region)
+    if err != nil {
+        return nil, err
+    }
+
+    request := mysql.GetConfigurationRequest{
+        ConfigurationId: types.String(id),
+        RequestMetadata: common.RequestMetadata{
+            RetryPolicy: getDefaultRetryPolicy(),
+        },
+    }
+
+    response, err := session.MySQLConfigurationClient.GetConfiguration(ctx, request)
+    if err != nil {
+        return nil, err
+    }
+
+    return response.Configuration, nil
+}

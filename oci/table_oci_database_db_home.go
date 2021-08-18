9f7e85eb5e603e465324ca84081d187f5dec9ce1
bigdatasourav <bigdatasourav@gmail.com>
2021-08-18 16:54:29 +0530

    WIP

---
+++ oci/table_oci_database_db_home.go
@@ -0,0 +1,365 @@
+package oci
+
+import (
+	"context"
+	"strings"
+
+	"github.com/oracle/oci-go-sdk/database"
+	"github.com/oracle/oci-go-sdk/v44/common"
+	"github.com/turbot/go-kit/types"
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+)
+
+//// TABLE DEFINITION
+
+func tableOciDatabaseDBHome(_ context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "oci_database_db_home",
+		Description: "OCI Database DB Home",
+		List: &plugin.ListConfig{
+			Hydrate: listDatabaseDBHomes,
+		},
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.SingleColumn("id"),
+			Hydrate:    getDatabaseDBHome,
+		},
+		GetMatrixItem: BuildCompartementRegionList,
+		Columns: []*plugin.Column{
+			{
+				Name:        "display_name",
+				Description: "The user-friendly name for the DB System. It does not have to be unique.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "id",
+				Description: "The OCID of the DB System.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromCamel(),
+			},
+			{
+				Name:        "lifecycle_state",
+				Description: "The current state of the DB System.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "configuration_id",
+				Description: "The OCID of the Configuration to be used for Instances in this DB System.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromCamel(),
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "subnet_id",
+				Description: "The OCID of the subnet the DB System is associated with.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromCamel(),
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "time_created",
+				Description: "The date and time the DB System was created.",
+				Type:        proto.ColumnType_TIMESTAMP,
+				Transform:   transform.FromField("TimeCreated.Time"),
+			},
+
+			// other columns
+			{
+				Name:        "availability_domain",
+				Description: "The Availability Domain where the primary DB System should be located.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "data_storage_size_in_gbs",
+				Description: "Initial size of the data volume in GiBs that will be created and attached.",
+				Type:        proto.ColumnType_INT,
+				Hydrate:     getMySQLDBSystem,
+				Transform:   transform.FromField("DataStorageSizeInGBs"),
+			},
+			{
+				Name:        "description",
+				Description: "User-provided data about the DB System.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "fault_domain",
+				Description: "The name of the fault domain the DB System is located in.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "hostname_label",
+				Description: "The hostname for the primary endpoint of the DB System.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "ip_address",
+				Description: "The IP address the DB System is configured to listen on.",
+				Type:        proto.ColumnType_IPADDR,
+				Hydrate:     getMySQLDBSystem,
+				Transform:   transform.FromField("IpAddress"),
+			},
+			{
+				Name:        "is_analytics_cluster_attached",
+				Description: "If the DB System has an Analytics Cluster attached.",
+				Type:        proto.ColumnType_BOOL,
+			},
+			{
+				Name:        "lifecycle_details",
+				Description: "Additional information about the current lifecycleState.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "mysql_version",
+				Description: "Name of the MySQL Version in use for the DB System.",
+				Type:        proto.ColumnType_STRING,
+			},
+			{
+				Name:        "port",
+				Description: "The port for primary endpoint of the DB System to listen on.",
+				Type:        proto.ColumnType_INT,
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "port_x",
+				Description: "The network port on which X Plugin listens for TCP/IP connections. This is the X Plugin equivalent of port.",
+				Type:        proto.ColumnType_INT,
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "shape_name",
+				Description: "The shape of the primary instances of the DB System.",
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "time_updated",
+				Description: "The time the DB System was last updated.",
+				Type:        proto.ColumnType_TIMESTAMP,
+				Transform:   transform.FromField("TimeUpdated.Time"),
+			},
+
+			// json fields
+			{
+				Name:        "analytics_cluster",
+				Description: "A summary of an Analytics Cluster.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "backup_policy",
+				Description: "BackupPolicy The Backup policy for the DB System.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "channels",
+				Description: "A list with a summary of all the Channels attached to the DB System.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "endpoints",
+				Description: "The network endpoints available for this DB System.",
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "maintenance",
+				Description: "The Maintenance Policy for the DB System.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getMySQLDBSystem,
+			},
+			{
+				Name:        "source",
+				Description: "DbSystemSource Parameters detailing how to provision the initial data of the DB System.",
+				Type:        proto.ColumnType_JSON,
+				Hydrate:     getMySQLDBSystem,
+			},
+
+			// tags
+			{
+				Name:        "defined_tags",
+				Description: ColumnDescriptionDefinedTags,
+				Type:        proto.ColumnType_JSON,
+			},
+			{
+				Name:        "freeform_tags",
+				Description: ColumnDescriptionFreefromTags,
+				Type:        proto.ColumnType_JSON,
+			},
+
+			// Standard Steampipe columns
+			{
+				Name:        "tags",
+				Description: ColumnDescriptionTags,
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.From(dbSystemTags),
+			},
+			{
+				Name:        "title",
+				Description: ColumnDescriptionTitle,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("DisplayName"),
+			},
+
+			// Standard OCI columns
+			{
+				Name:        "region",
+				Description: ColumnDescriptionRegion,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Id").Transform(ociRegionName),
+			},
+			{
+				Name:        "compartment_id",
+				Description: ColumnDescriptionCompartment,
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("CompartmentId"),
+			},
+			{
+				Name:        "tenant_id",
+				Description: ColumnDescriptionTenant,
+				Type:        proto.ColumnType_STRING,
+				Hydrate:     getTenantId,
+				Transform:   transform.FromValue(),
+			},
+		},
+	}
+}
+
+//// LIST FUNCTION
+
+func listDatabaseDBHomes(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	logger := plugin.Logger(ctx)
+	region := plugin.GetMatrixItem(ctx)[matrixKeyRegion].(string)
+	compartment := plugin.GetMatrixItem(ctx)[matrixKeyCompartment].(string)
+	logger.Debug("listDatabaseDBHomes", "Compartment", compartment, "OCI_REGION", region)
+
+	// Create Session
+	session, err := databaseService(ctx, d, region)
+	if err != nil {
+		return nil, err
+	}
+
+	request := database.ListDbHomesRequest{
+		CompartmentId: types.String(compartment),
+		RequestMetadata: common.RequestMetadata{
+			RetryPolicy: getDefaultRetryPolicy(),
+		},
+	}
+
+	pagesLeft := true
+	for pagesLeft {
+		response, err := session.DatabaseClient.ListDbHomes(ctx, request)
+		if err != nil {
+			return nil, err
+		}
+
+		for _, dbHome := range response.Items {
+			d.StreamListItem(ctx, dbHome)
+		}
+		if response.OpcNextPage != nil {
+			request.Page = response.OpcNextPage
+		} else {
+			pagesLeft = false
+		}
+	}
+
+	return nil, err
+}
+
+//// HYDRATE FUNCTION
+
+func getDatabaseDBHome(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	logger := plugin.Logger(ctx)
+	region := plugin.GetMatrixItem(ctx)[matrixKeyRegion].(string)
+	compartment := plugin.GetMatrixItem(ctx)[matrixKeyCompartment].(string)
+	logger.Debug("getDatabaseDBHome", "Compartment", compartment, "OCI_REGION", region)
+
+	var id string
+	if h.Item != nil {
+		id = *h.Item.(database.DbHomeSummary).Id
+	} else {
+		id = d.KeyColumnQuals["id"].GetStringValue()
+		// Restrict the api call to only root compartment/ per region
+		if !strings.HasPrefix(compartment, "ocid1.tenancy.oc1") {
+			return nil, nil
+		}
+	}
+
+	// handle empty id in get call
+	if id == "" {
+		return nil, nil
+	}
+
+	// Create Session
+	session, err := databaseService(ctx, d, region)
+	if err != nil {
+		return nil, err
+	}
+
+	request := database.GetDbHomeRequest{
+		DbSystemId: types.String(id),
+		RequestMetadata: common.RequestMetadata{
+			RetryPolicy: getDefaultRetryPolicy(),
+		},
+	}
+
+	response, err := session.DatabaseClient.GetDbHome(ctx, request)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.DbHome, nil
+}
+
+//// TRANSFORM FUNCTION
+
+func dbHomeTags(_ context.Context, d *transform.TransformData) (interface{}, error) {
+	freeformTags := dbSystemFreeformTags(d.HydrateItem)
+
+	var tags map[string]interface{}
+
+	if freeformTags != nil {
+		tags = map[string]interface{}{}
+		for k, v := range freeformTags {
+			tags[k] = v
+		}
+	}
+
+	definedTags := dbSystemDefinedTags(d.HydrateItem)
+
+	if definedTags != nil {
+		if tags == nil {
+			tags = map[string]interface{}{}
+		}
+		for _, v := range definedTags {
+			for key, value := range v {
+				tags[key] = value
+			}
+
+		}
+	}
+
+	return tags, nil
+}
+
+func dbHomeFreeformTags(item interface{}) map[string]string {
+	switch item := item.(type) {
+	case database.DbHome:
+		return item.FreeformTags
+	case database.DbHomeSummary:
+		return item.FreeformTags
+	}
+	return nil
+}
+
+func dbHomeDefinedTags(item interface{}) map[string]map[string]interface{} {
+	switch item := item.(type) {
+	case database.DbHome:
+		return item.DefinedTags
+	case database.DbHomeSummary:
+		return item.DefinedTags
+	}
+	return nil
+}

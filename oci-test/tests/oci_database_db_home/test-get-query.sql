7a92bea9eda27eed563b4c023b078c89f532e8f5
bigdatasourav <bigdatasourav@gmail.com>
2021-08-21 11:51:45 +0530

    update

--- oci-test/tests/oci_database_db_home/test-get-query.sql
+++
@@ -1,3 +0,0 @@
-select display_name, id, lifecycle_state
-from oci.oci_database_db_home
-where id = '{{ output.resource_id.value }}';
--- oci-test/tests/oci_database_db_home/test-not-found-query.sql
+++
@@ -1,3 +0,0 @@
-select id
-from oci.oci_database_db_home
-where id = '{{ output.resource_id.value }}aa';

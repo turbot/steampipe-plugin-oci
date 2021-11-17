d0390082b7851f893e34806346d8c9527a209bb4
ParthaI <partha@turbot.com>
2021-11-17 15:57:39 +0530

    Update

--- go.mod
+++ go.mod
@@ -1,10 +1,10 @@
 module github.com/turbot/steampipe-plugin-oci
 
-go 1.16
+go 1.17
 
 require (
 	github.com/oracle/oci-go-sdk/v44 v44.0.0
 	github.com/turbot/go-kit v0.3.0
-	github.com/turbot/steampipe-plugin-sdk v1.8.0
+	github.com/turbot/steampipe-plugin-sdk v1.8.1-beta.0
 	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b // indirect
 )
--- oci-test/test.sh
+++ oci-test/test.sh
@@ -30,67 +30,67 @@ run_test () {
  date >> failed_tests.txt
  date >> passed_tests.txt
 
-# run_test oci_apigateway_api
-# run_test oci_autoscaling_auto_scaling_configuration
-# run_test oci_budget_alert_rule
-# run_test oci_budget_budget
-# run_test oci_cloud_guard_configuration
-# run_test oci_cloud_guard_detector_recipe
-# run_test oci_cloud_guard_managed_list
-# run_test oci_cloud_guard_responder_recipe
-# run_test oci_cloud_guard_target
-# run_test oci_core_block_volume_replica
-# run_test oci_core_boot_volume
-# run_test oci_core_boot_volume_attachment
-# run_test oci_core_boot_volume_backup
-# run_test oci_core_boot_volume_replica
-# run_test oci_core_dhcp_options
-# run_test oci_core_drg
-# run_test oci_core_image
-# run_test oci_core_image_custom
-# run_test oci_core_instance
-# run_test oci_core_internet_gateway
-# run_test oci_core_load_balancer
-# run_test oci_core_local_peering_gateway
-# run_test oci_core_nat_gateway
-# run_test oci_core_network_load_balancer
-# run_test oci_core_network_security_group
-# run_test oci_core_public_ip
-# run_test oci_core_public_ip_pool
-# run_test oci_core_route_table
-# run_test oci_core_security_list
-# run_test oci_core_service_gateway
-# run_test oci_core_subnet
-# run_test oci_core_vcn
-# run_test oci_core_volume
-# run_test oci_core_volume_attachment
-# run_test oci_core_volume_backup
-# run_test oci_core_volume_backup_policy
-# run_test oci_database_autonomous_database
-# run_test oci_database_software_image
-# run_test oci_dns_rrset
-# run_test oci_dns_tsig_key
-# run_test oci_dns_zone
-# run_test oci_events_rule
-# run_test oci_file_storage_file_system
-# run_test oci_file_storage_mount_target
-# run_test oci_file_storage_snapshot
-# run_test oci_functions_application
-# run_test oci_identity_api_key
-# run_test oci_identity_auth_token
-# run_test oci_identity_availability_domain
-# run_test oci_identity_customer_secret_key
-# run_test oci_identity_dynamic_group
-# run_test oci_identity_network_source
-# run_test oci_identity_policy
-# run_test oci_identity_tag_default
-# run_test oci_identity_tag_namespace
-# run_test oci_identity_tenancy
-# run_test oci_kms_key
-# run_test oci_kms_key_version
-# run_test oci_kms_vault
-# run_test oci_logging_log
-# run_test oci_logging_log_group
+run_test oci_apigateway_api
+run_test oci_autoscaling_auto_scaling_configuration
+run_test oci_budget_alert_rule
+run_test oci_budget_budget
+run_test oci_cloud_guard_configuration
+run_test oci_cloud_guard_detector_recipe
+run_test oci_cloud_guard_managed_list
+run_test oci_cloud_guard_responder_recipe
+run_test oci_cloud_guard_target
+run_test oci_core_block_volume_replica
+run_test oci_core_boot_volume
+run_test oci_core_boot_volume_attachment
+run_test oci_core_boot_volume_backup
+run_test oci_core_boot_volume_replica
+run_test oci_core_dhcp_options
+run_test oci_core_drg
+run_test oci_core_image
+run_test oci_core_image_custom
+run_test oci_core_instance
+run_test oci_core_internet_gateway
+run_test oci_core_load_balancer
+run_test oci_core_local_peering_gateway
+run_test oci_core_nat_gateway
+run_test oci_core_network_load_balancer
+run_test oci_core_network_security_group
+run_test oci_core_public_ip
+run_test oci_core_public_ip_pool
+run_test oci_core_route_table
+run_test oci_core_security_list
+run_test oci_core_service_gateway
+run_test oci_core_subnet
+run_test oci_core_vcn
+run_test oci_core_volume
+run_test oci_core_volume_attachment
+run_test oci_core_volume_backup
+run_test oci_core_volume_backup_policy
+run_test oci_database_autonomous_database
+run_test oci_database_software_image
+run_test oci_dns_rrset
+run_test oci_dns_tsig_key
+run_test oci_dns_zone
+run_test oci_events_rule
+run_test oci_file_storage_file_system
+run_test oci_file_storage_mount_target
+run_test oci_file_storage_snapshot
+run_test oci_functions_application
+run_test oci_identity_api_key
+run_test oci_identity_auth_token
+run_test oci_identity_availability_domain
+run_test oci_identity_customer_secret_key
+run_test oci_identity_dynamic_group
+run_test oci_identity_network_source
+run_test oci_identity_policy
+run_test oci_identity_tag_default
+run_test oci_identity_tag_namespace
+run_test oci_identity_tenancy
+run_test oci_kms_key
+run_test oci_kms_key_version
+run_test oci_kms_vault
+run_test oci_logging_log
+run_test oci_logging_log_group
 run_test oci_mysql_backup
 run_test oci_mysql_channel
 run_test oci_mysql_configuration

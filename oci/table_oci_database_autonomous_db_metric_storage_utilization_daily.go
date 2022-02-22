de6027eea13fbc3df694c38d184269f745f68c91
ParthaI <partha@turbot.com>
2022-02-22 12:56:14 +0530

    Shorten the oci_database_autonomous_database_metric_storage_utilization_hourly and oci_database_autonomous_database_metric_storage_utilization_daily table names closes #362

--- oci/table_oci_database_autonomous_db_metric_storage_utilization_daily.go
+++ oci/table_oci_database_autonomous_db_metric_storage_utilization_daily.go
@@ -12,13 +12,13 @@ import (
 
 //// TABLE DEFINITION
 
-func tableOciDatabaseAutonomousDatabaseMetricStorageUtilizationHourly(_ context.Context) *plugin.Table {
+func tableOciDatabaseAutonomousDatabaseMetricStorageUtilizationDaily(_ context.Context) *plugin.Table {
 	return &plugin.Table{
-		Name:        "oci_database_autonomous_database_metric_storage_utilization_hourly",
-		Description: "OCI Autonomous Database Monitoring Metrics - Storage Utilization (Hourly)",
+		Name:        "oci_database_autonomous_db_metric_storage_utilization_daily",
+		Description: "OCI Autonomous Database Monitoring Metrics - Storage Utilization (Daily)",
 		List: &plugin.ListConfig{
 			ParentHydrate: listAutonomousDatabases,
-			Hydrate:       listAutonomousDatabaseMetricStorageUtilizationHourly,
+			Hydrate:       listAutonomousDatabaseMetricStorageUtilizationDaily,
 		},
 		GetMatrixItem: BuildCompartementRegionList,
 		Columns: MonitoringMetricColumns(
@@ -33,8 +33,8 @@ func tableOciDatabaseAutonomousDatabaseMetricStorageUtilizationHourly(_ context.
 	}
 }
 
-func listAutonomousDatabaseMetricStorageUtilizationHourly(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+func listAutonomousDatabaseMetricStorageUtilizationDaily(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
 	database := h.Item.(database.AutonomousDatabaseSummary)
 	region := fmt.Sprintf("%v", ociRegionNameFromId(*database.Id))
-	return listMonitoringMetricStatistics(ctx, d, "HOURLY", "oci_autonomous_database", "StorageUtilization", "resourceId", *database.Id, *database.CompartmentId, region)
+	return listMonitoringMetricStatistics(ctx, d, "DAILY", "oci_autonomous_database", "StorageUtilization", "resourceId", *database.Id, *database.CompartmentId, region)
 }

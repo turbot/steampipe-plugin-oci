a69adfb2819b101de5e1d40e9b219ce130ec8c67
ParthaI <partha@turbot.com>
2022-04-18 12:33:12 +0530

    Update the tenancy id for trigger intg test

--- oci-test/tests/oci_mysql_backup/variables.tf
+++ oci-test/tests/oci_mysql_backup/variables.tf
@@ -6,6 +6,7 @@ variable "resource_name" {
 
 variable "tenancy_ocid" {
   type        = string
+  default = "ocid1.tenancy.oc1..aaaaaaaahnm7gleh5soecxzjetci3yjjnjqmfkr4po3hoz4p4h2q37cyljaq"
   description = "OCID of your tenancy."
 }
 
--- oci-test/tests/oci_mysql_heat_wave_cluster/variables.tf
+++ oci-test/tests/oci_mysql_heat_wave_cluster/variables.tf
@@ -18,7 +18,7 @@ variable "heat_wave_cluster_cluster_size" {
 
 variable "tenancy_ocid" {
   type        = string
-  default     = ""
+  default     = "ocid1.tenancy.oc1..aaaaaaaahnm7gleh5soecxzjetci3yjjnjqmfkr4po3hoz4p4h2q37cyljaq"
   description = "OCID of your tenancy."
 }
 

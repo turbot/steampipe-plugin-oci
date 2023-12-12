3c211d656a134b55712bbf4a2c1f2b420361a479
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-13 01:22:05 +0530

    Recompile plugin with steampipe-plugin-sdk v5.8.0 (#574)
    Co-authored-by: Madhushree Ray <madhushree@turbot.com>

--- oci/connection_config.go
+++ oci/connection_config.go
@@ -2,62 +2,21 @@ package oci
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type ociConfig struct {
-	Auth                  *string  `cty:"auth"`
-	ConfigPath            *string  `cty:"config_path"`
-	Fingerprint           *string  `cty:"fingerprint"`
-	PrivateKey            *string  `cty:"private_key"`
-	PrivateKeyPassword    *string  `cty:"private_key_password"`
-	PrivateKeyPath        *string  `cty:"private_key_path"`
-	Profile               *string  `cty:"config_file_profile"`
-	Regions               []string `cty:"regions"`
-	TenancyOCID           *string  `cty:"tenancy_ocid"`
-	UserOCID              *string  `cty:"user_ocid"`
-	MaxErrorRetryAttempts *int     `cty:"max_error_retry_attempts"`
-	MinErrorRetryDelay    *int     `cty:"min_error_retry_delay"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"regions": {
-		Type: schema.TypeList,
-		Elem: &schema.Attribute{Type: schema.TypeString},
-	},
-	"auth": {
-		Type: schema.TypeString,
-	},
-	"tenancy_ocid": {
-		Type: schema.TypeString,
-	},
-	"config_file_profile": {
-		Type: schema.TypeString,
-	},
-	"config_path": {
-		Type: schema.TypeString,
-	},
-	"user_ocid": {
-		Type: schema.TypeString,
-	},
-	"fingerprint": {
-		Type: schema.TypeString,
-	},
-	"private_key": {
-		Type: schema.TypeString,
-	},
-	"private_key_path": {
-		Type: schema.TypeString,
-	},
-	"private_key_password": {
-		Type: schema.TypeString,
-	},
-	"max_error_retry_attempts": {
-		Type: schema.TypeInt,
-	},
-	"min_error_retry_delay": {
-		Type: schema.TypeInt,
-	},
+	Auth                  *string  `hcl:"auth"`
+	ConfigPath            *string  `hcl:"config_path"`
+	Fingerprint           *string  `hcl:"fingerprint"`
+	PrivateKey            *string  `hcl:"private_key"`
+	PrivateKeyPassword    *string  `hcl:"private_key_password"`
+	PrivateKeyPath        *string  `hcl:"private_key_path"`
+	Profile               *string  `hcl:"config_file_profile"`
+	Regions               []string `hcl:"regions,optional"`
+	TenancyOCID           *string  `hcl:"tenancy_ocid"`
+	UserOCID              *string  `hcl:"user_ocid"`
+	MaxErrorRetryAttempts *int     `hcl:"max_error_retry_attempts"`
+	MinErrorRetryDelay    *int     `hcl:"min_error_retry_delay"`
 }
 
 func ConfigInstance() interface{} {

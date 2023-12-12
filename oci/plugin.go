4ea79db8951c024347e52f6912efe2d02a56eda9
Madhushree Ray <madhushree@turbot.com>
2023-12-13 00:30:17 +0530

    Remove config schema

--- oci/plugin.go
+++ oci/plugin.go
@@ -25,7 +25,6 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 		},
 		ConnectionConfigSchema: &plugin.ConnectionConfigSchema{
 			NewInstance: ConfigInstance,
-			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
 			"oci_adm_knowledge_base":                                       tableAdmKnowledgeBase(ctx),
